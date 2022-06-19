250917b7a9a3041f95a689d049b17b3f1625c3be
Nick <83893877+MortyHub@users.noreply.github.com>
2022-06-19 17:00:33 +0000

    creating GUI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import tkinter as tk
 
 # Sends an error to the user of the program
 def error(text="Unknown Problem Has Occured"):
@@ -48,8 +49,21 @@ def clearData(name):
         with open(f'data/{name}.txt', 'r+') as file:
             file.truncate(0)
             
-# finds the average number of a list
+# Finds the average number of a list
 def finAv(lis):
     return sum(lis) / len(lis)
+
+# left off here
+
+window = tk.Tk()
+
+# Objects
+Create = tk.Button(window, text="Create Data", font=("Times New Roman", 10))
+Edit = tk.Button(window, text="Add Data", font=("Times New Roman", 10))
+Check = tk.Button(window, text="Check differences", font=("Times New Roman", 10))
+Exit = tk.Button(window,text="Exit", command=exit)
+Title = tk.Text(window,text="Spending Tracker", font=("Times New Roman", 16))
+
+# Displaying All Objects
 
 
